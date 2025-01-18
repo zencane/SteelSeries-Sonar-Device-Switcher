0e59ed3ee5d5105df08f184d18ffe7c5e13ede30
zencane <zenrycane@gmail.com>
2025-01-18 19:39:32 +0000

    update the way deviceId is acquired. trialing on one file

--- DeviceID/deviceIdUpdateHeadset.py
+++ DeviceID/deviceIdUpdateHeadset.py
@@ -28,6 +28,7 @@ else:
 for port in ports:
     # Command prefix for the PUT request
     cmd_command = (f"""curl http://127.0.0.1:{port}/streamRedirections/""")
+    print(cmd_command)
     # Define your command as a string
     # Use subprocess.run() to execute the command
     try:
@@ -35,9 +36,11 @@ for port in ports:
         json_string = str(result.stdout)
         # Parse the JSON string into a Python dictionary
         data = json.loads(json_string)
+
         # Extract the value
-        deviceId = str(data[1]['deviceId'])
-        pattern = r'\{[0-9a-fA-F:.]+\}\.\{([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})\}'
+        monitoring_device = next(item for item in data if item['streamRedirectionId'] == 'monitoring')
+        deviceId = monitoring_device['deviceId'].split('.')[-1].strip('{}')
+        pattern = r'([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})'
         match = re.search(pattern, deviceId)
         if match:
             correctId = match.group(1)
@@ -46,6 +49,5 @@ for port in ports:
         break
     except:
         pass
-
 
 x = input("press enter to exit.")
